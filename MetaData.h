ccde26f4e3a80a82cfc78ea4fae04db96832d304
Владислав Симонов <simonov.2019@azure.nstu.ru>
2021-03-15 13:00:50 +0700

    lw2

---
+++ MetaData.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <string>
+
+struct meta_head
+{
+	unsigned int num_level;
+	unsigned short int status;
+	std::string name;
+	std::string name_method;
+};
+
+class MetaData
+{
+	meta_head* array[];
+	
+};
--- main.cpp
+++ main.cpp
@@ -1,30 +1,9 @@
 ﻿#include <iostream>
 
-#include "VirtualArray.h"
+#include "MetaData.h"
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	srand(time(0));
-	int size = 30 + rand() % 1000;
-	int index[30];
-	VirtualArray* a = VirtualArray::GetInstance(size); //Создаём объект (используется паттерн "Одиночка")
-
-	std::cout << "Размер: " << size << std::endl;
 	
-	std::cout << "Записанные значения\n";
-	for (int& i : index) //Записываем числа
-	{
-		i = 0 + rand() % size;
-		int value = 0 + rand() % 1000;
-		a->write_var(i, value);
-		std::cout << "[" << i  << "]: "<< value << std::endl;
-	}//
-	
-	std::cout << "Считанные значения\n";
-	for (int& i : index) //Считываем числа
-		std::cout << "[" << i << "]: " << a->read_var(i) << std::endl;
-	
-	delete a;
-	return 0;
 }
